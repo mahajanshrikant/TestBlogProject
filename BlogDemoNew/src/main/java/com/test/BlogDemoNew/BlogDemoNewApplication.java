f488de4f2986f52040b4dd176b54a29a64769f0a
shrikant mahajan <shrikantmahajan10@gmail.com>
2025-01-08 19:31:55 +0530

    blog backend service

---
+++ BlogDemoNew/src/main/java/com/test/BlogDemoNew/BlogDemoNewApplication.java
@@ -0,0 +1,13 @@
+package com.test.BlogDemoNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDemoNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDemoNewApplication.class, args);
+	}
+
+}
